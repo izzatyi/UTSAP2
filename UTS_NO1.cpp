f024ec29073361590781316296cd5bdc83838173
izzatyi <nalizaizzati@gmail.com>
2024-04-23 14:19:53 +0700

    UTS_AP2_[076]

--- UTS_NO1.cpp
+++ UTS_NO1.cpp
@@ -4,71 +4,76 @@ using namespace std;
 
 int main()
 {
-    string nama, nilai_dalam_huruf, nim, mata_kuliah;
+    string nama, nilai_dalam_huruf, dosen_pa,  nim, mata_kuliah;
     char kom;
-    float nilai;
+    int hadir;
+    float tugas, uts, uas, quiz, nilai_akhir;
 
     system("CLS");
     //dibawah ini saya memasukkan spasi agar output yang ditampilkan rapi
     //karena faktanya, menggunakan \t tidak membuat mereka sejajar nantinya, oleh karena itu saya menggunakan spasi
-    cout << "Masukkan Nama        : "   ; getline(cin, nama); // menggunakan getline agar semua kata yang diinput bisa ditampilkan kembali
-    cout << "Masukkan NIM         : "   ; getline(cin, nim);
-    cout << "Masukkan Mata Kuliah : "   ; getline(cin, mata_kuliah);
-    cout << "Masukkan KOM         : "   ; cin >> kom;
-    cout << "Masukkan Nilai Tugas : "   ; cin >> nilai;
-
+    cout << "Masukkan Nama                   : "; getline(cin, nama); // menggunakan getline agar semua kata yang diinput bisa ditampilkan kembali
+    cout << "Masukkan NIM                    : "; getline(cin, nim);
+    cout << "Masukkan Nama Dosen PA          : "; getline(cin, dosen_pa);
+    cout << "Masukkan Mata Kuliah            : "; getline(cin, mata_kuliah);    
+    cout << "Masukkan KOM                    : "; cin >> kom;
+    cout << "Masukkan Jumlah Kehadiran (/14) : "; cin >> hadir;
+    cout << "Masukkan Nilai Tugas            : "; cin >> tugas;
+    cout << "Masukkan Nilai Quiz             : "; cin >> quiz;
+    cout << "Masukkan Nilai UTS              : "; cin >> uts;
+    cout << "Masukkan Nilai UAS              : "; cin >> uas;
+    
+    nilai_akhir = (0.15 * hadir) + (0.3 * uas) + (0.3 * uts) + (0.25 * tugas); //rumus untuk nilai akhir
+    
     //proses untuk mengubah dari nilai yang berbentuk angka kedalam huruf
-    if (nilai >= 0 && nilai <= 40)
+    if (nilai_akhir >= 0 && nilai_akhir <= 40)
     {
         nilai_dalam_huruf = "E";
     }
-    else if (nilai >= 41 && nilai <= 50)
+    else if (nilai_akhir >= 41 && nilai_akhir <= 50)
     {
         nilai_dalam_huruf = "D";
     }
-    else if  (nilai >= 51 && nilai <= 54)
+    else if  (nilai_akhir >= 51 && nilai_akhir <= 54)
     {
         nilai_dalam_huruf = "C-";
     }
-    else if (nilai >= 55 && nilai <= 59)
+    else if (nilai_akhir >= 55 && nilai_akhir <= 59)
     {
         nilai_dalam_huruf = "C";
     }
-    else if (nilai >= 60 && nilai <= 64)
+    else if (nilai_akhir >= 60 && nilai_akhir <= 64)
     {
         nilai_dalam_huruf = "C+";
     }
-    else if (nilai >= 65 && nilai <= 69)
+    else if (nilai_akhir >= 65 && nilai_akhir <= 69)
     {
         nilai_dalam_huruf = "B-";
     }
-    else if (nilai >= 70 && nilai <= 74)
+    else if (nilai_akhir >= 70 && nilai_akhir <= 74)
     {
         nilai_dalam_huruf = "B";
     }
-    else if  (nilai >= 75 && nilai <=79)
+    else if  (nilai_akhir >= 75 && nilai_akhir <= 80)
     {
         nilai_dalam_huruf = "B+";
     }
-    else if (nilai >= 80 && nilai <= 84)
-    {
-        nilai_dalam_huruf = "A-";
-    }
-    else if  (nilai >= 85 && nilai <= 100)
+    else if  (nilai_akhir >= 81 && nilai_akhir <= 100)
     {
         nilai_dalam_huruf = "A";
     }
-    else 
+    else if (nilai_akhir >= 101)
     {
-        nilai_dalam_huruf = "-"; //jika inputan yang diberikan lebih dari 100, maka program akan menampilkan "-"
+        nilai_dalam_huruf = "unvalid"; //jika inputan yang diberikan lebih dari 100, maka program akan menampilkan "-"
     }
 
     system("CLS"); // dibawah ini untuk menampilkan hasil akhir seperti yang diminta
     cout << "Nama        : " << nama              << endl;
     cout << "NIM         : " << nim               << endl;
     cout << "KOM         : " << kom               << endl;
+    cout << "Dosen PA    : " << dosen_pa          << endl;
     cout << "Mata Kuliah : " << mata_kuliah       << endl;
-    cout << "Nilai       : " << nilai_dalam_huruf << endl;
-    
+    cout << "Predikat    : " << nilai_dalam_huruf << endl;
+    cout << "Nilai Akhir : " << nilai_akhir;
     return 0;
 }
--- UTS_NO4.cpp
+++ UTS_NO4.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-using namespace std;
+using namespace std; // di soal hanya 'using std' seharusnya seperti disamping'
 
 int main()
 {
+    //system("CLS"); disini saya tdk memanggil clearscreen di awal.
+
     string umur; //di soal, nama variabelnya "a". Supaya lebih mudah dipahami, saya ganti variabel menjadi umur untuk menampung inputan dari user
 
     a : //label untuk kembali ke permainan jika user salah menebak umur
-    cout << "Tebak Umur Saya : "; 
-    cin >> umur;
+    cout << "Tebak Umur Saya : "; //di soal ga ada titik koma (';')
+    cin >> umur; //di soal, tanda nya terbalik '<<' seharusnya '>>'
     system("CLS"); //untuk membersihkan layar agar tidak terlalu penuh jika user melakukan 271 T percobaan
 
-    if (umur == "20")
+    if (umur == "20") //karena tipe data umur berupa string, maka harus pake petik dua (" ")
     {
         cout << umur <<  "\nJawaban anda benar!!\n";
     }
-    else 
+    else //lebih efektif menggunakan else daripada else if
     {
-        cout << umur << "\nJawaban anda salah. Coba lagi..\n";
+        cout << umur << "\nJawaban anda salah. Coba lagi..\n"; //di soal tidak ada titik koma (;) dan 'endl' saya ubah menjadi '\n'
         goto a;
     }
 
